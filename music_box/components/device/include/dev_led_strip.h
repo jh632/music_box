d58f1018432303c82f6b7011a184b0cc7e503715
Nano <c1218n@qq.com>
2026-06-05 20:40:15 +0800

    add led

--- music_box/components/device/include/dev_led_strip.h
+++ music_box/components/device/include/dev_led_strip.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
-#include "hal_rmt.h"
+#include <stdlib.h>
 #include "esp_err.h"
+#include "esp_log.h"
+#include "hal_gpio.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,25 +14,21 @@ extern "C" {
 typedef struct dev_led_strip_s *dev_led_strip_handle_t;
 
 typedef struct {
-    gpio_num_t gpio_num;
-    uint32_t resolution_hz;
-    size_t mem_block_symbols;
-    size_t trans_queue_depth;
-    uint32_t bit0_high_ns;
-    uint32_t bit0_low_ns;
-    uint32_t bit1_high_ns;
-    uint32_t bit1_low_ns;
-    uint32_t reset_ns;
-    uint32_t num_pixels;
+    const gpio_num_t *gpio_nums;
+    uint32_t          led_count;
+    uint8_t           active_level;
 } dev_led_strip_config_t;
 
 typedef struct {
     esp_err_t (*init)(const dev_led_strip_config_t *cfg, dev_led_strip_handle_t *out);
     esp_err_t (*deinit)(dev_led_strip_handle_t h);
-    esp_err_t (*set_pixel)(dev_led_strip_handle_t h, uint32_t index, uint8_t r, uint8_t g, uint8_t b);
-    esp_err_t (*refresh)(dev_led_strip_handle_t h);
-    esp_err_t (*clear)(dev_led_strip_handle_t h);
-    esp_err_t (*set_brightness)(dev_led_strip_handle_t h, uint8_t brightness);
+    esp_err_t (*on)(dev_led_strip_handle_t h, uint32_t index);
+    esp_err_t (*off)(dev_led_strip_handle_t h, uint32_t index);
+    esp_err_t (*set)(dev_led_strip_handle_t h, uint32_t index, bool on);
+    esp_err_t (*all_on)(dev_led_strip_handle_t h);
+    esp_err_t (*all_off)(dev_led_strip_handle_t h);
+    esp_err_t (*toggle)(dev_led_strip_handle_t h, uint32_t index);
+    esp_err_t (*chase_step)(dev_led_strip_handle_t h);
 } dev_led_strip_ops_t;
 
 const dev_led_strip_ops_t *dev_led_strip_get_ops(void);
