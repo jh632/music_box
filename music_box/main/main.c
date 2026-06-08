4f3983f640ce59107dddad8b2b876b79ca9aa2d7
Nano <c1218n@qq.com>
2026-06-08 18:34:14 +0800

    GPIO,audio,新加开启PSRAM和内存打印，测试代码保留

--- music_box/main/main.c
+++ music_box/main/main.c
@@ -1,27 +1,50 @@
-/*
- * main.c — 编排入口
- *
- * 只负责层级初始化调用，不出现任何具体模块的配置参数。
- * 具体初始化逻辑由各层的 _init_all() 负责。
- */
+#include "app_audio_verify.h"
 
+#include "esp_heap_caps.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "nvs_flash.h"
 
 static const char *TAG = "MAIN";
 
+// 打印内存使用情况
+void displayMemoryUsage() {
+    size_t totalDRAM = heap_caps_get_total_size(MALLOC_CAP_INTERNAL);
+    size_t freeDRAM = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
+    size_t usedDRAM = totalDRAM - freeDRAM;
+
+    size_t totalPSRAM = heap_caps_get_total_size(MALLOC_CAP_SPIRAM);
+    size_t freePSRAM = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+    size_t usedPSRAM = totalPSRAM - freePSRAM;
+
+    size_t DRAM_largest_block = heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL);
+    size_t PSRAM_largest_block = heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM);
+
+    float dramUsagePercentage = (float)usedDRAM / totalDRAM * 100;
+    float psramUsagePercentage = (float)usedPSRAM / totalPSRAM * 100;
+
+    ESP_LOGI(TAG, "DRAM Total: %zu bytes, Used: %zu bytes, Free: %zu bytes,  DRAM_Largest_block: %zu bytes", totalDRAM, usedDRAM, freeDRAM, DRAM_largest_block);
+    ESP_LOGI(TAG, "DRAM Used: %.2f%%", dramUsagePercentage);
+    ESP_LOGI(TAG, "PSRAM Total: %zu bytes, Used: %zu bytes, Free: %zu bytes, PSRAM_Largest_block: %zu bytes", totalPSRAM, usedPSRAM, freePSRAM, PSRAM_largest_block);
+    ESP_LOGI(TAG, "PSRAM Used: %.2f%%", psramUsagePercentage);
+} 
+
 void app_main(void)
 {
-    /* ESP-IDF 系统组件 */
+    /* main 只做系统初始化和应用层入口编排。 */
     ESP_ERROR_CHECK(nvs_flash_init());
 
-    /*
-     * 分层初始化（顺序即依赖顺序）
-     * TODO: 待各层实现后逐步解注释
-     *
-     * ESP_ERROR_CHECK(dev_init_all());
-     * ESP_ERROR_CHECK(app_init_all());
-     */
+    esp_err_t ret = app_audio_verify_run();
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "audio verify failed: %s", esp_err_to_name(ret));
+        return;
+    }
 
-    ESP_LOGI(TAG, "Music Box started.");
+    ESP_LOGI(TAG, "audio verify finished.");
+
+    while (true) {
+        displayMemoryUsage(); //只有测试音乐结束才能打印，还没优化
+        vTaskDelay(pdMS_TO_TICKS(5 * 1000));
+    }
 }
